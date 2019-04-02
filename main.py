dc450e7fa4e0075dd7728ab158a45db40d7bb1bb
Andrew Dixon <infinite_number@yahoo.com>
2019-04-02 01:25:45 -0500

    added jinja

--- main.py
+++ main.py
@@ -1,46 +1,19 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-header = """
-    <!DOCTYPE html>
-    <html>
-        <header>
-            <h1>My Awesome Website</h1>
-        </header>
-"""
-
-signup_body = """
-        <body>
-            <h2>Signup Sheet</h2>
-            <form action="/authenticate" method="post">
-                <label>
-                    Username: <input type="text" name="username" />
-                </label>
-                <label>
-                    Password: <input type="password" name="password1" />
-                </label>
-                <label>Re-type Password: 
-                    <input type="text" name="password2" />
-                </label>
-                <label>
-                    E-mail Address: <input type="email" name="email" />
-                </label>
-            </form>
-        </body>
-"""
-
-footer = """
-        <footer>
-        <p>The Experience Enterprises</p>
-        </footer>
-    </html>
-"""
 
 @app.route("/")
 def index():
-    return header + signup_body + footer
+    template = jinja_env.get_template('signup_page.html')
+    return template.render()
 
 @app.route("/authenticate")
 def validate():
